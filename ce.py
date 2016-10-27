e0d80b60b091bf09085ff6ee53d435657d91fcc5
casperenghuus <cne@Caspers-MacBook-Pro.local>
2016-10-27 18:04:56 -0400

    ce.py

--- ce.py
+++ ce.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 
 import os
-import subprocess
 import pandas as pd
 
-f_Loayza = 'data/GSE42509_PQST_rna_rp_fpkms.txt.gz'
+f_Loayza = 'data/GSE42509_PQST_rna_rp_fpkms.txt'
 CWD = os.getcwd()
 
 def read_Loayza():
-	# Load path and escape characters
+	'''Load data from Loayza paper into DataFrame'''
 	f = os.path.join(CWD, f_Loayza)
-	# f = f.replace('(', '\\(').replace(')', '\\)').replace(' ', '\\ ')
 
-	# Pipe .gz file
-	command = 'gunzip %s'%f
 	handle = subprocess.Popen(command, shell=True)
-	df = pd.read_table(f[:-3])
-	print df.head(50)
+	df = pd.read_table(f)
+	return df
 
 df = read_Loayza()
 
