85bce71a90fe02d1a0c598bf1e8e8a0df0d3abd9
casperenghuus <cne@dhcp-18-111-107-45.dyn.mit.edu>
2016-10-28 11:09:59 -0400

    ce.py

--- ce.py
+++ ce.py
@@ -2,6 +2,8 @@
 
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 f_Loayza = 'data/GSE42509_PQST_rna_rp_fpkms.txt'
 CWD = os.getcwd()
@@ -12,6 +14,30 @@ def read_Loayza():
 	df = pd.read_table(f)
 	return df
 
+def heatmap(df, fn, L=None):
+	'''Plot heatmap of dataframe'''
+	# Make gene names into
+	df = df.set_index('sym')
+	# Sort by sum of rows
+	idx = df.sum(axis=1).sort_values(ascending=False).index
+
+	# Plot
+	fig, ax = plt.subplots(figsize=(10,30))
+	# Plot subset of data?
+	if L != None:
+		sns.heatmap(df.ix[idx][0:L])
+	else:
+		sns.heatmap(df.ix[idx])
+	# Annotations
+	plt.xticks(rotation=45)
+	plt.yticks(rotation=0)
+	plt.ylabel('Gene Symbol')
+	plt.xlabel('Experiment')
+
+	# Save figure
+	plt.savefig(fn)
+
 df = read_Loayza()
+heatmap(df.drop(['enstxids'], axis=1), 'heatmap.png', 100)
 
 
