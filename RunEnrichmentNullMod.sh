a153f9338fb26574324f7f91761e88d9cc54118d
jchtt <jchtt@arcor.de>
2016-12-09 18:14:48 -0500

    Update

--- RunEnrichmentNullMod.sh
+++ RunEnrichmentNullMod.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 opdir=data/red-sd
-python2 EnrichmentNullMod.py --comm_size_max 20 --comm_size_step 10 --outfile $opdir/p_cut_offs.csv --iters 5 --msigprefix Gene4x/msigdb_filtered --nodes $opdir/nodes.txt
+python2 EnrichmentNullMod.py --comm_size_max 500 --comm_size_step 10 --outfile $opdir/p_cut_offs.csv --iters 500 --msigprefix Gene4x/msigdb_filtered --nodes $opdir/nodes.txt
