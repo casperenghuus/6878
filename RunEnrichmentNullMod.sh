32a65c8376cfa83b5a883043c427c1a2dfd59405
jchtt <jchtt@arcor.de>
2016-12-11 12:05:52 -0500

    New Enrichment model

--- RunEnrichmentNullMod.sh
+++ RunEnrichmentNullMod.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-opdir=data/red-new
-outname=p_cut_offs_2.csv
+opdir=data/full-new
+outname=p_cut_offs_3.csv
 # pvals=$(seq 1 9; seq 10 10 100; seq 100 50 500;)
 pvals=$(seq 600 100 1000; seq 1250 250 3000)
 # pvals="1 2"
