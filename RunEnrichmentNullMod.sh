658be90cc77846ff7f7738cdceb50d6c2195cc0c
jchtt <jchtt@arcor.de>
2016-12-10 20:31:56 -0500

    Updated enrichment null model

--- RunEnrichmentNullMod.sh
+++ RunEnrichmentNullMod.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-opdir=data/full-sd
+opdir=data/red-new
 outname=p_cut_offs.csv
-# pvals=$(seq 1 9; seq 10 10 100; seq 100 50 500;)
+pvals=$(seq 1 9; seq 10 10 100; seq 100 50 500;)
 # pvals=$(seq 1 9)
-pvals="1 2"
+# pvals="1 2"
 echo $pvals
 python2 EnrichmentNullMod.py $pvals --outfile $opdir/$outname --iters 500 --msigprefix Gene4x/msigdb_filtered --nodes $opdir/nodes.txt
