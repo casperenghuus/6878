3640d779f6723bb09bac49effb6dbf4b5b43519f
jchtt <jchtt@arcor.de>
2016-12-11 12:25:56 -0500

    Biclustering update

--- RunBiclustering.py
+++ RunBiclustering.py
@@ -5,13 +5,13 @@ from joblib import Parallel, delayed
 def execCommand(x):
     subprocess.call(x, shell = True)
 
-execCommand('python2 BipartiteClustering.py -fout biCl_red-new -p -po red-new_noClust_def-Clusters -pc red-new_def-Clusters')
+# execCommand('python2 BipartiteClustering.py -fout biCl_red-new -p -po red-new_noClust_def-Clusters -pc red-new_def-Clusters')
 
 commandlist = []
 
-sizes = [190, 170, 150, 110, 90, 70, 50]
+# sizes = [190, 170, 150, 110, 90, 70, 50]
+sizes = [300, 400, 500, 600, 700]
 for size in sizes:
-    commandlist.append('python2 BipartiteClustering.py -fout biCl_red-new_c{} -c 190'.format(size, size))
+    commandlist.append('python2 BipartiteClustering.py -fout biCl_red-new_c{} -c {}'.format(size, size))
 
-
-Parallel(n_jobs = 24)(delayed(execCommand)(x) for x in commandlist)
+Parallel(n_jobs = 1)(delayed(execCommand)(x) for x in commandlist)
