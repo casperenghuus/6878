7f47fa36efa4ebd195055badfb1ed4819c703788
jchtt <jchtt@arcor.de>
2016-12-12 16:47:06 -0500

    ...

--- RunCompareClusters.sh
+++ RunCompareClusters.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-opdir=red-sd
-./CompareClusters.py data/$opdir/slice_out_oslom/slice_0/results_{1,2,3}/tp --outfile data/$opdir/nmis.csv
+opdir=data/red-new
+# clusterfiles=$(find $opdir \( -wholename '*multi_cluster/clusters_*.txt' -o -iname 'biCl*.csv' -o -wholename '*slice_out_oslom/consensus/tp' \) ! \( -iname '*sym.txt' -o -iname '*enr*.csv' \) -exec bash -c 'printf "%q " "$@"' dummy '{}' +)
+clusterfiles=$(find $opdir \( -wholename '*multi_cluster/clusters_*phi_0800.txt' -o -iname 'biCl*900*.csv' -o -iname 'biCl*300*.csv' -o -wholename '*slice_out_oslom/consensus/tp' -o -wholename '*slice_out_oslom/slice_*/results_1/tp' \) ! \( -iname '*sym.txt' -o -iname '*enr*.csv' \) -exec bash -c 'printf "%q " "$@"' dummy '{}' +)
+./CompareClustersNMI.py $clusterfiles --outfile $opdir/nmi.csv
