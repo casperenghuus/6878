63bb1495fd371853189079abddb8ad93cc51cc57
jchtt <jchtt@arcor.de>
2016-12-11 20:43:13 -0500

    ...

--- RunClusterEnrichment.sh
+++ RunClusterEnrichment.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 opdir=data/red-new
-clusterfiles=$(find $opdir \( -wholename '*multi_cluster/clusters_*.txt' -o -iname 'biCl*.csv' -o -wholename '*slice_out_oslom/consensus/tp' \) ! \( -iname '*sym.txt' -o -iname '*enr*.csv' \) -exec bash -c 'printf "%q " "$@"' dummy '{}' +)
+# clusterfiles=$(find $opdir \( -wholename '*multi_cluster/clusters_*.txt' -o -iname 'biCl*.csv' -o -wholename '*slice_out_oslom/consensus/tp' \) ! \( -iname '*sym.txt' -o -iname '*enr*.csv' \) -exec bash -c 'printf "%q " "$@"' dummy '{}' +)
+clusterfiles=$(find $opdir \( -iname 'biCl*800*.csv' -o -iname 'biCl*900*.csv' \) ! \( -iname '*sym.txt' -o -iname '*enr*.csv' \) -exec bash -c 'printf "%q " "$@"' dummy '{}' +)
 # clusterfiles=$(find $opdir \( -iname 'biCl*.csv' -o -wholename '*slice_out_oslom/consensus/tp' \) ! \( -iname '*sym.txt' -o -iname '*enr*.csv' \) -exec bash -c 'printf "%q " "$@"' dummy '{}' +)
 # clusterfiles=$(find $opdir \( -iname 'biCl*.csv' -o -wholename '*slice_out_oslom/consensus/tp' \) ! \( -iname '*sym.txt' -o -iname '*enr*.csv' \) -exec bash -c 'printf "%q " "$@"' dummy '{}' +)
 # clusterfiles=$opdir/biCl_red_sd_c25_BiClustering_tp.csv
 # echo $clusterfiles
-python2 ClusterEnrichment.py $clusterfiles --nodes $opdir/nodes.txt --msigprefix Gene4x/msigdb/ --outfile $opdir/overview_3.csv --graphs $opdir/net_* --thresholds $opdir/p_cut_offs.csv
+python2 ClusterEnrichment.py $clusterfiles --nodes $opdir/nodes.txt --msigprefix Gene4x/msigdb/ --outfile $opdir/overview_4.csv --graphs $opdir/net_* --thresholds $opdir/p_cut_offs.csv
